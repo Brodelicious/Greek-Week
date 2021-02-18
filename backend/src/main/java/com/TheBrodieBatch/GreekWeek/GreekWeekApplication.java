9574a11ddb73149014e5c21c2fc73453d2cf12a2
bhufnagel <Brodinator@Live.com>
2021-02-17 22:39:59 -0500

    Changed sring boot build

---
+++ backend/src/main/java/com/TheBrodieBatch/GreekWeek/GreekWeekApplication.java
@@ -0,0 +1,13 @@
+package com.TheBrodieBatch.GreekWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreekWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreekWeekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/TheBrodieBatch/GreekWeek/GreekWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheBrodieBatch.GreekWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreekWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
